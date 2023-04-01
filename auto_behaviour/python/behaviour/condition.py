87a0cd6f4431860083fcd9ac25e5906df63d56b5
Shaswat2001 <sis_shaswat@outlook.com>
2023-04-01 17:16:48 +0530

    Added youbot for testing behaviour tree

--- auto_behaviour/python/behaviour/condition.py
+++ auto_behaviour/python/behaviour/condition.py
@@ -1,9 +1,12 @@
 import py_trees
+from nav_msgs.msg import Odometry
 
 class IsMobileAtPose(py_trees.behaviour.Behaviour):
 
-    def __init__(self, name):
+    def __init__(self, name,pose):
+
         super(IsMobileAtPose,self).__init__(name)
+        self.pose = pose
 
     def setup(self):
         pass
